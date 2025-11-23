beba100fbfdc5499a747648bbb23f33da2e0f9ae
lester <nazarethlester@gmail.com>
2025-11-23 21:51:05 +0530

    new_changes

--- frontend/src/assets/images/Image.ts
+++ frontend/src/assets/images/Image.ts
@@ -1,8 +1,11 @@
-import aboutus from './nav/aboutus.svg'
+import AboutUs from './nav/AboutUs.svg'
 import Project from './nav/Projects.svg'
-
+import Contact from './nav/Contacts.svg'
+import Experience from './nav/Experience.svg'
 // Export them in an object
 export const images = {
-  aboutus,
-  Project
+  AboutUs,
+  Project,
+  Contact,
+  Experience,
 };
